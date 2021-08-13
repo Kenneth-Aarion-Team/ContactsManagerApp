87cb8329ea98b20362ccefb2d8bd9db749a91e5c
deceir <83772817+deceir@users.noreply.github.com>
2021-08-13 15:03:56 -0500

    moved app over to contacts class

--- src/Main.java
+++ src/Main.java
@@ -11,78 +11,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        Contacts contacts = new Contacts();
 
-        Input input = new Input();
-
-        String directory = "data";
-        String filename = "contacts.txt";
-
-        ArrayList<String> contactsContents = new ArrayList<>();
-
-        Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory, filename);
-
-        if (Files.notExists(dataDirectory)) {
-            Files.createDirectories(dataDirectory);
-        }
-
-        if (Files.notExists(dataFile)) {
-            Files.createFile(dataFile);
-        }
-
-        // Boolean used to determine if application should keep running
-
-        boolean userWantsToContinue = true;
-
-        // Initial application running, prints existing contacts
-
-        System.out.println("Welcome to the ContactManagementSolutions App.");
-        System.out.println("Here is your current list of contacts.");
-        System.out.println(contactsContents);
-
-        // Initial user selection
-
-        do {
-
-            System.out.println("Please enter select from the options below.");
-            System.out.println("-view contacts");
-            System.out.println("-add contacts");
-            System.out.println("-search contacts");
-            System.out.println("-delete contact");
-            System.out.println("-exit program");
-
-            String userSelection = input.getString();
-
-            // View
-            if (userSelection.toLowerCase().contains("view")) {
-                System.out.println("The user selected to view.");
-            }
-
-            // Add
-            if (userSelection.toLowerCase().contains("add")) {
-                System.out.println("The user selected to add.");
-            }
-
-            // Search
-            if (userSelection.toLowerCase().contains("search")) {
-                System.out.println("The user selected to search.");
-            }
-
-            // Delete
-            if (userSelection.toLowerCase().contains("delete")) {
-                System.out.println("The user selected to delete.");
-            }
-
-            // Exit
-            if (userSelection.toLowerCase().contains("delete")) {
-                System.out.println("The user selected to exit.");
-                userWantsToContinue = false;
-            }
-
-        } while (userWantsToContinue);
-
-
+        contacts.ContactsApp();
 
     }
 
